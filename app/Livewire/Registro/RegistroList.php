5accaddcbea2e85d5c84e941ad6a4ed44f923814
vitindadao <victordadao@hotmail.com>
2025-09-16 13:46:24 -0300

    listagem de registro

--- app/Livewire/Registro/RegistroList.php
+++ app/Livewire/Registro/RegistroList.php
@@ -2,12 +2,34 @@
 
 namespace App\Livewire\Registro;
 
+use App\Models\Registro;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class RegistroList extends Component
 {
+
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
+
+    public $search = '';
+    public $perPage = 15;
+ 
+    protected $queryString = [
+        'search' => ['except' => ''],
+        'perPage' => ['except' => 15],
+    ];
+ 
     public function render()
     {
-        return view('livewire.registro.registro-list');
+        $registro = Registro::where('tipo', 'like', "{$this->search}%")
+        ->orWhere('valor', 'like', "{$this->search}%")
+        ->orWhere('unidade', 'like', "{$this->search}%")
+        ->orWhere('data_hora', 'like', "{$this->search}%")
+        ->paginate($this->perPage);
+
+        return view('livewire.registro.registro-list', compact('registros'));
+
     }
+
 }
