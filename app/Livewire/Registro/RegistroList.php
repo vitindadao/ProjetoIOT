76b0e0c0ad97cf2739847a4a1fa5be96e508bac5
vitindadao <victordadao@hotmail.com>
2025-09-16 13:38:27 -0300

    crud de registro

---
+++ app/Livewire/Registro/RegistroList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Registro;
+
+use Livewire\Component;
+
+class RegistroList extends Component
+{
+    public function render()
+    {
+        return view('livewire.registro.registro-list');
+    }
+}
